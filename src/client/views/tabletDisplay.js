2d721188995e05437e561faf57bde7f48ceca56e
Bradley Faskowitz <Bradley Faskowitz>
2017-04-11 00:47:45 -0500

    Added dialog to tabletView

--- src/client/views/tabletDisplay.js
+++ src/client/views/tabletDisplay.js
@@ -9,31 +9,44 @@
 import React from 'react';
 import { Link } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
-import {Card, CardText} from 'material-ui/Card'
+import {Card, CardText} from 'material-ui/Card';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
+//let path = require('path');
+//console.log(path.join(__dirname, '/dispense/dispense.js'));
 //let gpio = require(path.join(__dirname, '../../dispense/dispense.js'));
+
 
 export class TabletDisplay extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             drinkList: undefined,
-            lastOrder: []
+            lastOrder: undefined,
+            thisOrder: undefined,
+            open: false
         };
-        this._getOrders = this._getOrders.bind(this)
+        this._getOrders = this._getOrders.bind(this);
+        this._handleDialogOpen = this._handleDialogOpen.bind(this);
+        this._handleDialogClose = this._handleDialogClose.bind(this);
+        this._handlePour = this._handlePour.bind(this);
     }
 
-    _handleClick(order, event) {
+    _handlePour() {
+        let order = this.state.thisOrder;
+        let url = "/order/" + order._id;
+
         $.ajax({
             type: "DELETE",
-            url: "/order/" + order._id,
+            url: url,
             success: function(data) {
                 $.ajax({
                     type: "GET",
                     url: "/orders",
                     success: function(data) {
-                        this.setState({"drinkList": data, "lastOrder": order});
-                        //gpio.doGpio;
+                        this.setState({"drinkList": data, "lastOrder": order, "thisOrder": undefined, "open": false});
+                        //gpio.doGpio();
                     }.bind(this),
                     error: function(error) {
                         console.log(error);
@@ -59,13 +72,35 @@ export class TabletDisplay extends React.Component {
         })
     }
 
+    _handleDialogOpen(order, event) {
+        console.log(order);
+        this.setState({thisOrder: order, open: true});
+    }
+
+    _handleDialogClose() {
+        this.setState({thisOrder: undefined, open: false});
+    }
+
     componentDidMount() {
         this._getOrders();
         setTimeout(this.componentDidMount.bind(this), 5000);
     }
 
     render() {
-        let lastPerson = this.state.lastOrder !== undefined && this.state.lastOrder.drinker !== undefined ?
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onTouchTap={this._handleDialogClose}
+            />,
+            <FlatButton
+                label="Pour"
+                primary={true}
+                onTouchTap={this._handlePour}
+            />,
+        ];
+
+        let lastPerson = this.state.lastOrder !== undefined && this.state.lastOrder.drinker !== "" ?
             " by " + this.state.lastOrder.drinker : null;
 
         let lastOrder = this.state.lastOrder === undefined ? null :
@@ -77,12 +112,20 @@ export class TabletDisplay extends React.Component {
                     let drinker = this.state.drinkList.orders[key].drinker !== "" ? " by " +
                                                         this.state.drinkList.orders[key].drinker : null;
                     let textKey = key + "text";
-                    return (<Card key={key} onTouchTap={this._handleClick.bind(this, this.state.drinkList.orders[key])}>
+                    return (<Card key={key} onTouchTap={this._handleDialogOpen.bind(this, this.state.drinkList.orders[key])}>
                                 <CardText key={textKey}>{this.state.drinkList.orders[key].drink}{drinker}</CardText>
                             </Card>)}.bind(this)));
         return (
             <div>
                 <Card>
+                    <Dialog
+                        actions={actions}
+                        modal={false}
+                        open={this.state.open}
+                        onRequestClose={this._handleDialogClose}
+                    >
+                        Is the drink ready to pour?
+                    </Dialog>
                     {lastOrder}
                     {view}
                 </Card>
